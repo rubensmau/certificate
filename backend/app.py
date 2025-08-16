7348e0ebc4e8eea16c5ff02d60382a4d119e60ff
Rubens Mau <rubensmau@gmail.com>
2025-08-16 17:26:34 +0300

    fix

--- backend/app.py
+++ backend/app.py
@@ -174,4 +174,4 @@ def test_db():
 init_db()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
